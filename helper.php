22fdad36603e6acba7ff84efb9cab8d4791afdb5
Ricardo Oficina <ricardolau6@gmail.com>
2016-11-08 21:47:37 +0100

    Primer commit

---
+++ helper.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_menu
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ * @since       1.5
+ */
+class ModMenusvHelper
+{
+	/**
+	 * Get a list of the menu items.
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  The module options.
+	 *
+	 * @return  array
+	 *
+	 * @since   1.5
+	 */
+	public static function getList(&$params)
+	{
+		$app = JFactory::getApplication();
+		$menu = $app->getMenu();
+
+		// Get active menu item
+		$base = self::getBase($params);
+		$user = JFactory::getUser();
+		$levels = $user->getAuthorisedViewLevels();
+		asort($levels);
+		$key = 'menu_items' . $params . implode(',', $levels) . '.' . $base->id;
+		$cache = JFactory::getCache('mod_menu', '');
+
+		if (!($items = $cache->get($key)))
+		{
+			$path    = $base->tree;
+			$start   = (int) $params->get('startLevel');
+			$end     = (int) $params->get('endLevel');
+			$showAll = $params->get('showAllChildren');
+			$items   = $menu->getItems('menutype', $params->get('menutype'));
+
+			$lastitem = 0;
+
+			if ($items)
+			{
+				foreach ($items as $i => $item)
+				{
+					if (($start && $start > $item->level)
+						|| ($end && $item->level > $end)
+						|| (!$showAll && $item->level > 1 && !in_array($item->parent_id, $path))
+						|| ($start > 1 && !in_array($item->tree[$start - 2], $path)))
+					{
+						unset($items[$i]);
+						continue;
+					}
+
+					$item->deeper     = false;
+					$item->shallower  = false;
+					$item->level_diff = 0;
+
+					if (isset($items[$lastitem]))
+					{
+						$items[$lastitem]->deeper     = ($item->level > $items[$lastitem]->level);
+						$items[$lastitem]->shallower  = ($item->level < $items[$lastitem]->level);
+						$items[$lastitem]->level_diff = ($items[$lastitem]->level - $item->level);
+					}
+
+					$item->parent = (boolean) $menu->getItems('parent_id', (int) $item->id, true);
+
+					$lastitem     = $i;
+					$item->active = false;
+					$item->flink  = $item->link;
+
+					// Reverted back for CMS version 2.5.6
+					switch ($item->type)
+					{
+						case 'separator':
+						case 'heading':
+							// No further action needed.
+							continue;
+
+						case 'url':
+							if ((strpos($item->link, 'index.php?') === 0) && (strpos($item->link, 'Itemid=') === false))
+							{
+								// If this is an internal Joomla link, ensure the Itemid is set.
+								$item->flink = $item->link . '&Itemid=' . $item->id;
+							}
+							break;
+
+						case 'alias':
+							$item->flink = 'index.php?Itemid=' . $item->params->get('aliasoptions');
+							break;
+
+						default:
+							$item->flink = 'index.php?Itemid=' . $item->id;
+							break;
+					}
+
+					if (strcasecmp(substr($item->flink, 0, 4), 'http') && (strpos($item->flink, 'index.php?') !== false))
+					{
+						$item->flink = JRoute::_($item->flink, true, $item->params->get('secure'));
+					}
+					else
+					{
+						$item->flink = JRoute::_($item->flink);
+					}
+
+					// Evitamos la doble codificación porque por alguna razón el $item es compartido para los módulos de menú y obtenemos doble codificación
+					// Cuando se encuenta la causa de que el argumento debe ser eliminado
+					$item->title        = htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8', false);
+					$item->anchor_css   = htmlspecialchars($item->params->get('menu-anchor_css', ''), ENT_COMPAT, 'UTF-8', false);
+					$item->anchor_title = htmlspecialchars($item->params->get('menu-anchor_title', ''), ENT_COMPAT, 'UTF-8', false);
+					$item->menu_image   = $item->params->get('menu_image', '') ?
+						htmlspecialchars($item->params->get('menu_image', ''), ENT_COMPAT, 'UTF-8', false) : '';
+
+				}
+
+				if (isset($items[$lastitem]))
+				{
+					$items[$lastitem]->deeper     = (($start?$start:1) > $items[$lastitem]->level);
+					$items[$lastitem]->shallower  = (($start?$start:1) < $items[$lastitem]->level);
+					$items[$lastitem]->level_diff = ($items[$lastitem]->level - ($start?$start:1));
+					
+				}
+			}
+
+			$cache->store($items, $key);
+			
+		}
+		
+		return $items;
+	}
+
+	/**
+	 * Get base menu item.
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  The module options.
+	 *
+	 * @return  object
+	 *
+	 * @since	3.0.2
+	 */
+	public static function getBase(&$params)
+	{
+		// Get base menu item from parameters
+		if ($params->get('base'))
+		{
+			$base = JFactory::getApplication()->getMenu()->getItem($params->get('base'));
+		}
+		else
+		{
+			$base = false;
+		}
+
+		// Use active menu item if no base found
+		if (!$base)
+		{
+			$base = self::getActive($params);
+		}
+
+		return $base;
+	}
+
+	/**
+	 * Get active menu item.
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  The module options.
+	 *
+	 * @return  object
+	 *
+	 * @since	3.0.2
+	 */
+	public static function getActive(&$params)
+	{
+		$menu = JFactory::getApplication()->getMenu();
+		$lang = JFactory::getLanguage();
+
+		// Look for the home menu
+		if (JLanguageMultilang::isEnabled())
+		{
+			$home = $menu->getDefault($lang->getTag());
+		}
+		else
+		{
+			$home  = $menu->getDefault();
+		}
+
+		return $menu->getActive() ? $menu->getActive() : $home;
+	}
+
+	public static function getMaximenu($list)
+	{
+		
+				$IdItemNivelInf= 0;
+				$contador = 0;
+				$listitems = $list;
+				foreach ($listitems as $item)
+				{
+
+				
+					if ($item->level == 2)
+					{
+					// Entramos cuando el item tiene nivel 2 ( el MAXIMENU)
+						
+						if ($IdItemNivelInf == 0 or $IdItemNivelInf= $item->tree[0]){
+						// Quiere decir que es el primer item de nivel 2
+						$IdItemNivelInf= $item->tree[0];
+						$contador = $contador +1;
+						} else {
+							$contador = 1;
+							$IdItemNivelInf= $item->tree[0];
+
+						}
+					
+					//~ echo 'Id'.$item->id.'Co'.$contador.'Ni'.$item->level.'NiAnt'.$IdItemNivelInf.'<br/>';
+					$columnasMaximenu[$IdItemNivelInf] = $contador;
+					
+										
+					}
+					if ($item->level == 1){
+					// Ponemos contador a 0
+					$contador = 0;
+					}
+					
+ 
+					
+					
+				}
+				
+	return $columnasMaximenu;
+	
+	
+	
+	
+	
+	}
+
+
+
+}
+
+
---
+++ mod_menusv.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Include the syndicate functions only once
+require_once __DIR__ . '/helper.php';
+
+
+// Carga de   CSS
+//Carga de ficheros .CSS y JS
+$doc = JFactory::getDocument();
+
+//$doc->addScript(JURI::base(true).'/modules/mod_carousel/lib/jquery.bxslider.min.js', 'text/javascript');
+$doc->addStyleSheet(JURI::base(true).'/modules/mod_menusv/css/maximenu.css');
+
+$list      = ModMenusvHelper::getList($params);
+$base      = ModMenusvHelper::getBase($params);
+$active    = ModMenusvHelper::getActive($params);
+
+$active_id = $active->id;
+$path      = $base->tree;
+$showAll   = $params->get('showAllChildren');
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+$ItMenuNi  = ModMenusvHelper::getMaximenu($list);
+
+if (count($list))
+{
+	require JModuleHelper::getLayoutPath('mod_menusv', $params->get('layout', 'default'));
+}
